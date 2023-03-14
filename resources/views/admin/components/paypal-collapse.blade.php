47b1d102dee2644fe23b691cc4e45070c68526ee
Pcbtroniks <desarrollo.software@pcbtroniks.com>
2023-03-14 10:18:52 -0600

    Fix\ Ortografia corregida en paypal collapse

--- resources/views/admin/components/paypal-collapse.blade.php
+++ resources/views/admin/components/paypal-collapse.blade.php
@@ -1,5 +1,5 @@
 <h5 class="my-5">
-    Al dar clic en "Suscribirse" seras dirijido a la plataforma de PayPal para realizar el pago de tu membresía.
+    Al dar clic en "Suscribirse" seras dirigido a la plataforma de PayPal para realizar el pago de tu membresía.
 <br>
 <br>
 <a href="https://www.paypal.com/mx/webapps/mpp/ua/privacy-full?locale.x=es_MX" target="_blank">Política de privacidad</a> | <a href="https://www.paypal.com/mx/webapps/mpp/ua/useragreement-full?locale.x=es_MX" target="_blank">Acuerdo de usuario</a>
